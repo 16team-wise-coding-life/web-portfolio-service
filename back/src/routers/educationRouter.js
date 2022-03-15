e538174474490b859917f784a395d343bb631dad
yuhayan <cnessit@gmail.com>
2022-03-16 01:39:09 +0900

    [Modify]: POST Router에 req.body.user_id 추가

--- back/src/routers/educationRouter.js
+++ back/src/routers/educationRouter.js
@@ -11,11 +11,13 @@ educationRouter.post('/education/create', login_required, async (req, res, next)
       throw new Error('headers의 Content-Type을 application/json으로 설정해주세요');
     }
 
+    const user_id = req.body.user_id;
     const school = req.body.school;
     const major = req.body.major;
     const position = req.body.position;
 
     const newEducation = await EducationService.addEducation({
+      user_id,
       school,
       major,
       position,
@@ -29,22 +31,21 @@ educationRouter.post('/education/create', login_required, async (req, res, next)
   } catch (error) {
     next(error);
   }
-
-  educationRouter.get('/educations/:id', login_required, async (req, res, next) => {
-    try {
-      const education_id = req.params.id;
-      const currentEducationInfo = await EducationService.getEducationInfo({
-        education_id,
-      });
-
-      if (currentEducationInfo.errorMessage) {
-        throw new Error(currentEducationInfo.errorMessage);
-      }
-      res.status(200).send(currentEducationInfo);
-    } catch (error) {
-      next(error);
-    }
-  });
 });
 
+educationRouter.get('/educations/:id', login_required, async (req, res, next) => {
+  try {
+    const education_id = req.params.id;
+    const currentEducationInfo = await EducationService.getEducationInfo({
+      education_id,
+    });
+
+    if (currentEducationInfo.errorMessage) {
+      throw new Error(currentEducationInfo.errorMessage);
+    }
+    res.status(200).send(currentEducationInfo);
+  } catch (error) {
+    next(error);
+  }
+});
 export { educationRouter };
