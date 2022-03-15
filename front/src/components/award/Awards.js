8172cbf47aa265058e306b144d2d8de1e79c0394
Seohyun-Roh <radult951@gmail.com>
2022-03-15 23:55:01 +0900

    [Add] : Awards 기본 UI 구현

--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 
-function Awards() {
+import Award from './Award';
+import AwardEditForm from './AwardEditForm';
+
+function Awards({ portfolioOwnerId, isEditable }) {
+  /* useEffect로 portfolioOwnerId로 수상이력 GET 요청 -> award를 res.data로 세팅하기 */
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <>
       <Card>
         <Card.Body>
           <Card.Title>수상이력</Card.Title>
-          {/*<Award />*/}
+
+          {/* props로 award, setIsEditing 넘기기
+          isEditing이 true-> AwardEditForm
+          false-> Award*/}
+          {isEditing ? <AwardEditForm setIsEditing={setIsEditing} /> : <Award setIsEditing={setIsEditing} />}
         </Card.Body>
       </Card>
     </>
