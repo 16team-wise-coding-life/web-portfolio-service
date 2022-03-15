e9c6aa0374efe774dc081462aab216d43fd1504c
yuhayan <cnessit@gmail.com>
2022-03-16 00:59:19 +0900

    [Add]: Education POST

---
+++ back/src/routers/educationRouter.js
@@ -0,0 +1,34 @@
+import is from '@sindresorhus/is';
+import { Router } from 'express';
+import { EducationService } from '../services/educationService';
+import { login_required } from '../middlewares/login_required';
+
+const educationRouter = Router();
+
+educationRouter.post('/education/create', login_required, async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error('headers의 Content-Type을 application/json으로 설정해주세요');
+    }
+
+    const school = req.body.school;
+    const major = req.body.major;
+    const position = req.body.position;
+
+    const newEducation = await EducationService.addEducation({
+      school,
+      major,
+      position,
+    });
+
+    if (newEducation.errorMessage) {
+      throw new Error(newEducation.errorMessage);
+    }
+
+    res.status(201).json(newEducation);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export { educationRouter };
--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -1,18 +1,21 @@
 import { Education } from '../db';
+import { v4 as uuidv4 } from 'uuid';
 
 class EducationService {
-  // 학력 추가
-  static async addEducation({ school, major, finalEducation }) {
-    const newEducation = { school, major, finalEducation };
+  static async addEducation({ user_id, school, major, position }) {
+    const education_id = uuidv4();
+    const newEducation = {
+      id: education_id,
+      user_id,
+      school,
+      major,
+      position,
+    };
+
     const createdNewEducation = await Education.create({ newEducation });
     createdNewEducation.errorMessage = null;
 
     return createdNewEducation;
-  }
-  // 사용자가 작성한 모든 학력 가져옴
-  static async getEducations() {
-    const educations = await Education.findAll();
-    return educations;
   }
 }
 
