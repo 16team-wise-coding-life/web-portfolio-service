3beba53facff114196413f0fd38119d0eb9c5c85
Seohyun-Roh <radult951@gmail.com>
2022-03-16 01:11:48 +0900

    [Add] : AwardAddForm 기본 UI 구현

--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form, Card, Col, Row } from 'react-bootstrap';
+import * as Api from '../../api';
 
-function AwardAddForm() {
-  return <div>AwardAddForm</div>;
+function AwardAddForm({ portfolioOwnerId, setIsAdding }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(title, description);
+    // portfolioOwnerId, title, description 정보 서버에 보내서 변경하기
+    // (POST -> award/create)
+    setIsAdding(false);
+  };
+
+  return (
+    <>
+      <Card className='mb-2'>
+        <Card.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId='useEditTitle'>
+              <Form.Control type='text' placeholder='수상내역' value={title} onChange={e => setTitle(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId='userEditDescription'>
+              <Form.Control type='text' placeholder='상세내역' value={description} onChange={e => setDescription(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group as={Row} className='mt-3 text-center'>
+              <Col>
+                <Button variant='primary' type='submit'>
+                  확인
+                </Button>
+                <Button variant='secondary' onClick={() => setIsAdding(false)}>
+                  취소
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
 export default AwardAddForm;
--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 
 import Award from './Award';
 import AwardEditForm from './AwardEditForm';
+import AwardAddForm from './AwardAddForm';
 
 function Awards({ portfolioOwnerId, isEditable }) {
-  /* useEffect로 portfolioOwnerId로 수상이력 GET 요청 -> award를 res.data로 세팅하기 */
+  /* useEffect로 portfolioOwnerId로 수상이력 GET -> awardlist/:user_id
+  award를 res.data로 세팅하기
+  const [awards, setAwards] = useState([]);
+  */
   const [isEditing, setIsEditing] = useState(false);
+  const [isAdding, setIsAdding] = useState(false);
 
   return (
     <>
@@ -18,6 +23,12 @@ function Awards({ portfolioOwnerId, isEditable }) {
           isEditing이 true-> AwardEditForm
           false-> Award*/}
           {isEditing ? <AwardEditForm setIsEditing={setIsEditing} /> : <Award setIsEditing={setIsEditing} />}
+          <Row className='mt-3 text-center mb-4'>
+            <Col>
+              <Button onClick={() => setIsAdding(true)}>+</Button>
+            </Col>
+          </Row>
+          {isAdding && <AwardAddForm portfolioOwnerId={portfolioOwnerId} setIsAdding={setIsAdding} />}
         </Card.Body>
       </Card>
     </>
