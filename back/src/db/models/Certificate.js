2f7b648e6ae70ff4ed91b6c546f522a0c2244583
parkjungyoon <tbr06057@naver.com>
2022-03-15 23:34:18 +0900

    [Add] Certificate 모델

--- back/src/db/models/Certificate.js
+++ back/src/db/models/Certificate.js
@@ -1,1 +1,10 @@
+import { CertificateModel } from '../schemas/certificate';
 
+class Certificate {
+  static async create({ newCertificate }) {
+    const createdCertificate = await CertificateModel.create(newCertificate);
+    return createdCertificate;
+  }
+}
+
+export { Certificate };
