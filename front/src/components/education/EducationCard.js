d90542c092e82146f5303e75ef296242787d4b8f
YounJoo Seog <reina.seog@gmail.com>
2022-03-16 20:19:45 +0900

    [Add] EducationCard 구현(미완)

--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 
-const EducationCard = () => {};
+const EducationCard = () => {
+  return (
+    <Card.Text>
+      <Row>
+        <Col>
+          <span>//학교명</span>
+          <br />
+          <span className="text-muted">//전공, 현재상태</span>
+        </Col>
+        <Col>
+          <Button variant="outline-info">편집</Button>
+        </Col>
+      </Row>
+    </Card.Text>
+  );
+};
 export default EducationCard;
