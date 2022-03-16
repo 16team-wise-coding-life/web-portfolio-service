7fbd39563233e4054be1369b66a8ac4f416bf993
YounJoo Seog <reina.seog@gmail.com>
2022-03-16 20:20:00 +0900

    [Add] Educations 구현(미완)

--- front/src/components/education/Educations.js
+++ front/src/components/education/Educations.js
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Button, Card, Col, Row } from 'react-bootstrap';
 import * as Api from '../../api';
 import Education from './Education';
-import EducationCard from './EducationCard';
-import EducationEditForm from './EducationEditForm';
+import EducationAddForm from './EducationAddForm';
 
 const Educations = (portfolioOwnerId, isEditable) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [education, setEducation] = useState([]);
+  const [isAdding, setIsAdding] = useState(false);
+  const [educations, setEducation] = useState([]);
 
   useEffect(() => {
     Api.get('educationlist', portfolioOwnerId).then(res => setEducation(res.data));
@@ -19,17 +18,19 @@ const Educations = (portfolioOwnerId, isEditable) => {
         <Card.Body>
           <Card.Title>학력</Card.Title>
           <Card.Text>
-            // 학력 리스트
-            {education.map(education => (
+            {educations.map(education => (
               <Education key={education.id} setEducation={setEducation} isEditable={isEditable} />
             ))}
-            // 추가 폼 들어갈 자리
           </Card.Text>
-          <Row>
-            <Col>
-              <Button></Button>
-            </Col>
-          </Row>
+
+          {isEditable && (
+            <Row>
+              <Col>
+                <Button onClick={() => setIsAdding(true)}>+</Button>
+              </Col>
+            </Row>
+          )}
+          {isAdding && <EducationAddForm portfolioOwnerId={portfolioOwnerId} setIsAdding={setIsAdding} setEducation={setEducation}></EducationAddForm>}
         </Card.Body>
       </Card>
     </>
