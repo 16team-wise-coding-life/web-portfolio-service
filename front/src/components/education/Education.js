0a42f984fd84b3c39e6f15c865ee9a6ca17661d7
YounJoo Seog <reina.seog@gmail.com>
2022-03-16 20:19:16 +0900

    [Add] Education 구현(미완)

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import EducationCard from './EducationCard';
 import EducationEditForm from './EducationEditForm';
 
-const Education = () => {
+const Education = (education, setEducation) => {
   const [isEditing, setIsEditing] = useState('false');
   return (
     <>
       {isEditing ? (
-          <EducationEditForm 
-          />
-      ) : ()}
+        <EducationEditForm currenteducation={education} setEducation={setEducation} setIsEditing={setIsEditing}></EducationEditForm>
+      ) : (
+        <EducationCard education={education} setEducation={setEducation} setIsEditing={setIsEditing}></EducationCard>
+      )}
     </>
   );
-}
+};
 
 export default Education;
