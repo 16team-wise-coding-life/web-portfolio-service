0599e849a004db2dbc2c67781232a2ec0e6f1736
Seohyun-Roh <radult951@gmail.com>
2022-03-16 17:08:23 +0900

    [Add] : Api 사용해 목록 구현

--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Button, Row, Col } from 'react-bootstrap';
 
 import Award from './Award';
+import AwardCard from './AwardCard';
 import AwardEditForm from './AwardEditForm';
 import AwardAddForm from './AwardAddForm';
+import * as Api from '../../api';
 
 function Awards({ portfolioOwnerId, isEditable }) {
-  /* useEffect로 portfolioOwnerId로 수상이력 GET -> awardlist/:user_id
-  award를 res.data로 세팅하기
-  const [awards, setAwards] = useState([]);
-  */
-  const [isEditing, setIsEditing] = useState(false);
   const [isAdding, setIsAdding] = useState(false);
+  const [awards, setAwards] = useState([]);
+
+  useEffect(() => {
+    Api.get(`awardlist/${portfolioOwnerId}`).then(res => setAwards(res.data));
+  }, []);
 
   return (
     <>
       <Card>
         <Card.Body>
           <Card.Title>수상이력</Card.Title>
-
-          {/* props로 award, setIsEditing 넘기기
-          isEditing이 true-> AwardEditForm
-          false-> Award*/}
-          {isEditing ? <AwardEditForm setIsEditing={setIsEditing} /> : <Award setIsEditing={setIsEditing} />}
+          {awards.map(a => {
+            return <AwardCard key={a._id} award={a} />;
+          })}
           <Row className='mt-3 text-center mb-4'>
             <Col>
               <Button onClick={() => setIsAdding(true)}>+</Button>
