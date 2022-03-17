efc3d466dcb25948c78149825bfe45973a4a5297
Seohyun-Roh <radult951@gmail.com>
2022-03-17 19:57:21 +0900

    [Add] CerticateAddForm 구현

--- front/src/components/certificate/Certificates.js
+++ front/src/components/certificate/Certificates.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Card, Button, Row, Col } from 'react-bootstrap';
 
 import CertificateCard from './CertificateCard';
+import CertificateAddForm from './CertificateAddForm';
+
 import * as Api from '../../api';
 
 function Certificates({ portfolioOwnerId, isEditable }) {
@@ -21,8 +23,11 @@ function Certificates({ portfolioOwnerId, isEditable }) {
             return <CertificateCard key={certificate._id} certificateCard={certificate} isEditable={isEditable} />;
           })}
           <Row className='mt-3 text-center mb-4'>
-            <Col>{/*<Button onClick={() => setIsAdding(true)}>+</Button>*/}</Col>
+            <Col>
+              <Button onClick={() => setIsAdding(true)}>+</Button>
+            </Col>
           </Row>
+          {isAdding && <CertificateAddForm certificates={certificates} setCertificates={setCertificates} portfolioOwnerId={portfolioOwnerId} setIsAdding={setIsAdding} />}
         </Card.Body>
       </Card>
     </>
