a488f44ce92c5e4c7960b6ccdb2438a12c7f23fa
Seohyun-Roh <radult951@gmail.com>
2022-03-18 00:08:19 +0900

    [Modify] Card 추가 권한 체크

--- front/src/components/certificate/Certificates.js
+++ front/src/components/certificate/Certificates.js
@@ -23,9 +23,7 @@ function Certificates({ portfolioOwnerId, isEditable }) {
             return <CertificateCard key={certificate._id} certificateCard={certificate} isEditable={isEditable} />;
           })}
           <Row className='mt-3 text-center mb-4'>
-            <Col>
-              <Button onClick={() => setIsAdding(true)}>+</Button>
-            </Col>
+            <Col>{isEditable && <Button onClick={() => setIsAdding(true)}>+</Button>}</Col>
           </Row>
           {isAdding && <CertificateAddForm certificates={certificates} setCertificates={setCertificates} portfolioOwnerId={portfolioOwnerId} setIsAdding={setIsAdding} />}
         </Card.Body>
