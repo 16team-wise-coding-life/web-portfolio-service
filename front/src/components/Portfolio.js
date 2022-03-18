b32eb4afb24bd3cb4c860218110f2b27dcf07ca3
YounJoo Seog <reina.seog@gmail.com>
2022-03-18 23:49:37 +0900

    [Add] MVP 4개 모두 포트폴리오에 업데이트

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -5,7 +5,10 @@ import { Container, Col, Row } from 'react-bootstrap';
 import { UserStateContext } from '../App';
 import * as Api from '../api';
 import User from './user/User';
+import Educations from './education/Educations';
 import Awards from './award/Awards';
+import Certificates from './certificate/Certificates';
+import Projects from './Project/Projects';
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -55,11 +58,14 @@ function Portfolio() {
   return (
     <Container fluid>
       <Row>
-        <Col md='3' lg='3'>
+        <Col md="3" lg="3">
           <User portfolioOwnerId={portfolioOwner.id} isEditable={portfolioOwner.id === userState.user?.id} />
         </Col>
         <Col>
+          <Educations portfolioOwnerId={portfolioOwner.id} isEditable={portfolioOwner.id === userState.user?.id} />
           <Awards portfolioOwnerId={portfolioOwner.id} isEditable={portfolioOwner.id === userState.user?.id} />
+          <Certificates portfolioOwnerId={portfolioOwner.id} isEditable={portfolioOwner.id === userState.user?.id} />
+          <Projects portfolioOwnerId={portfolioOwner.id} isEditable={portfolioOwner.id === userState.user?.id} />
         </Col>
       </Row>
     </Container>
