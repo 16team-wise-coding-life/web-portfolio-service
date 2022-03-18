8d1e932c8f182a23def318b8a179ba59b562e433
yuhayan <cnessit@gmail.com>
2022-03-18 15:00:25 +0900

    [Modify]: 학력 추가할 때 id값 불필요하여 제거

--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -1,11 +1,8 @@
 import { Education } from '../db';
-import { v4 as uuidv4 } from 'uuid';
 
 class EducationService {
   static async addEducation({ user_id, school, major, position }) {
-    const education_id = uuidv4();
     const newEducation = {
-      id: education_id,
       user_id,
       school,
       major,
