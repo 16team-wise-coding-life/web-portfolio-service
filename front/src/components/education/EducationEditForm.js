a7802dcba9f9a8c12c3bed3951be85ad812d1a7e
YounJoo Seog <reina.seog@gmail.com>
2022-03-19 11:07:33 +0900

    [Modify] 15줄 아직 해결 전 상태

--- front/src/components/education/EducationEditForm.js
+++ front/src/components/education/EducationEditForm.js
@@ -12,8 +12,7 @@ function EducationEditForm({ currentEducation, setEducations, setIsEditing }) {
 
     const user_id = currentEducation.user_id;
     console.log('user_id', user_id);
-    await Api.put(`educations/${currentEducation.user_id}`, {
-      user_id,
+    await Api.put(`educations/${currentEducation.id}`, {
       school,
       major,
       position,
@@ -25,27 +24,27 @@ function EducationEditForm({ currentEducation, setEducations, setIsEditing }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="educationEditSchool" className="mt-3">
-        <Form.Control type="text" placeholder="학교 이름" value={school} onChange={e => setSchool(e.target.value)} />
+      <Form.Group controlId='educationEditSchool' className='mt-3'>
+        <Form.Control type='text' placeholder='학교 이름' value={school} onChange={e => setSchool(e.target.value)} />
       </Form.Group>
-      <Form.Group controlId="educationEditMajor" className="mt-3">
-        <Form.Control type="text" placeholder="전공" value={major} onChange={e => setMajor(e.target.value)} />
+      <Form.Group controlId='educationEditMajor' className='mt-3'>
+        <Form.Control type='text' placeholder='전공' value={major} onChange={e => setMajor(e.target.value)} />
       </Form.Group>
 
-      <div key={`inline-radio`} className="mb-3 mt-3">
-        <Form.Check inline label="재학중" id="radio-edit1" type="radio" name="position" value="재학중" checked={position === '재학중'} onChange={e => setPosition(e.target.value)} />
-        <Form.Check inline label="학사졸업" id="radio-edit2" type="radio" name="position" value="학사졸업" checked={position === '학사졸업'} onChange={e => setPosition(e.target.value)} />
-        <Form.Check inline label="석사졸업" id="radio-edit3" type="radio" name="position" value="석사졸업" checked={position === '석사졸업'} onChange={e => setPosition(e.target.value)} />
-        <Form.Check inline label="박사졸업" id="radio-edit4" type="radio" name="position" value="박사졸업" checked={position === '박사졸업'} onChange={e => setPosition(e.target.value)} />
+      <div key={`inline-radio`} className='mb-3 mt-3'>
+        <Form.Check inline label='재학중' id='radio-edit1' type='radio' name='position' value='재학중' checked={position === '재학중'} onChange={e => setPosition(e.target.value)} />
+        <Form.Check inline label='학사졸업' id='radio-edit2' type='radio' name='position' value='학사졸업' checked={position === '학사졸업'} onChange={e => setPosition(e.target.value)} />
+        <Form.Check inline label='석사졸업' id='radio-edit3' type='radio' name='position' value='석사졸업' checked={position === '석사졸업'} onChange={e => setPosition(e.target.value)} />
+        <Form.Check inline label='박사졸업' id='radio-edit4' type='radio' name='position' value='박사졸업' checked={position === '박사졸업'} onChange={e => setPosition(e.target.value)} />
       </div>
 
-      <Form.Group as={Row} className="mt-3 text-center">
+      <Form.Group as={Row} className='mt-3'>
         <Row>
           <Col>
-            <Button variant="primary" type="submit" className="me-2">
+            <Button variant='primary' type='submit'>
               확인
             </Button>
-            <Button variant="secondary" type="submit" onClick={() => setIsEditing(false)}>
+            <Button variant='secondary' onClick={() => setIsEditing(false)}>
               취소
             </Button>
           </Col>
