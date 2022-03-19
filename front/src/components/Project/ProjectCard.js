99611f0dd4fbdb94b3637cb50cef517f47e0f12c
YounJoo Seog <reina.seog@gmail.com>
2022-03-19 18:22:48 +0900

    Revert "[Modify] ProjectCard CSS 수정 및 구현"
    This reverts commit 9e024d141ce46b5e863dd0234778c6ff2ae869d8.

--- front/src/components/Project/ProjectCard.js
+++ front/src/components/Project/ProjectCard.js
@@ -3,20 +3,20 @@ import { Button, Card, Col, Row } from 'react-bootstrap';
 
 function ProjectCard({ project, setIsEditing, isEditable }) {
   return (
-    <Card.Text className='mb-2 mt-3'>
-      <Row className='align-items-center'>
+    <Card.Text>
+      <Row className="align-items-center">
         <Col>
           {project.title}
           <br />
-          <span className='text-muted'>
+          <span className="text-muted">
             {`${project.description}`}
             <br />
             {`${project.from_date.split('T')[0]} ~ ${project.to_date.split('T')[0]}`}
           </span>
         </Col>
         {isEditable && (
-          <Col xs='auto' lg='1'>
-            <Button className='mr-3' variant='outline-info' size='sm' onClick={() => setIsEditing(edit => !edit)}>
+          <Col xs lg="1">
+            <Button variant="outline-info" onClick={() => setIsEditing(edit => !edit)}>
               편집
             </Button>
           </Col>
