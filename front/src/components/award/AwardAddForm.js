60cfc207da4a18435c07a5eeb56e7fe4c3d2584f
Seohyun Roh <radult951@gmail.com>
2022-03-19 10:42:49 +0900

    [Modify] Card 삭제

--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -21,30 +21,26 @@ function AwardAddForm({ awards, setAwards, portfolioOwnerId, setIsAdding }) {
 
   return (
     <>
-      <Card className='mb-2'>
-        <Card.Body>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId='awardEditTitle'>
-              <Form.Control type='text' placeholder='수상내역' value={title} onChange={e => setTitle(e.target.value)} />
-            </Form.Group>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId='awardEditTitle'>
+          <Form.Control type='text' placeholder='수상내역' value={title} onChange={e => setTitle(e.target.value)} />
+        </Form.Group>
 
-            <Form.Group controlId='awardEditDescription'>
-              <Form.Control type='text' placeholder='상세내역' value={description} onChange={e => setDescription(e.target.value)} />
-            </Form.Group>
+        <Form.Group controlId='awardEditDescription'>
+          <Form.Control type='text' placeholder='상세내역' value={description} onChange={e => setDescription(e.target.value)} />
+        </Form.Group>
 
-            <Form.Group as={Row} className='mt-3 text-center'>
-              <Col>
-                <Button variant='primary' type='submit'>
-                  확인
-                </Button>
-                <Button variant='secondary' onClick={() => setIsAdding(false)}>
-                  취소
-                </Button>
-              </Col>
-            </Form.Group>
-          </Form>
-        </Card.Body>
-      </Card>
+        <Form.Group as={Row} className='mt-3 text-center'>
+          <Col>
+            <Button variant='primary' type='submit'>
+              확인
+            </Button>
+            <Button variant='secondary' onClick={() => setIsAdding(false)}>
+              취소
+            </Button>
+          </Col>
+        </Form.Group>
+      </Form>
     </>
   );
 }
