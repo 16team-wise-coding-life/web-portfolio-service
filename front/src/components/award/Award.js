465136718e3da84db9407015be70e93783902e22
Seohyun-Roh <radult951@gmail.com>
2022-03-22 12:38:51 +0900

    [Add] Delete 기능 구현

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -3,11 +3,19 @@ import React, { useState } from 'react';
 import AwardCard from './AwardCard';
 import AwardEditForm from './AwardEditForm';
 
-function Award({ awardCard, isEditable }) {
+function Award({ awardCard, isEditable, setIsDeleted }) {
   const [isEditing, setIsEditing] = useState(false);
   const [award, setAward] = useState({ title: awardCard.title, description: awardCard.description, id: awardCard._id });
 
-  return <>{isEditing ? <AwardEditForm award={award} setAward={setAward} setIsEditing={setIsEditing} /> : <AwardCard award={award} setIsEditing={setIsEditing} isEditable={isEditable} />}</>;
+  return (
+    <>
+      {isEditing ? (
+        <AwardEditForm award={award} setAward={setAward} setIsEditing={setIsEditing} />
+      ) : (
+        <AwardCard award={award} setIsEditing={setIsEditing} isEditable={isEditable} setIsDeleted={setIsDeleted} />
+      )}
+    </>
+  );
 }
 
 export default Award;
