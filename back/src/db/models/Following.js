7c3c268495272b5363012894fd442219a265bda5
yuhayan <cnessit@gmail.com>
2022-03-23 15:56:59 +0900

    [Add]: following Delete

--- back/src/db/models/Following.js
+++ back/src/db/models/Following.js
@@ -5,6 +5,11 @@ class Following {
     const following = await FollowingModel.find({ user_id: user_id });
     return following;
   }
+
+  static async deleteFollowing({ unfollow }) {
+    const deletedFollowing = await FollowingModel.findOneAndDelete(unfollow);
+    return deletedFollowing;
+  }
 }
 
 export { Following };
