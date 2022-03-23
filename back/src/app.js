22c853f0c395e11c299c3ff201794b7949627dac
sohyun-k <sohyun.kwon@yonsei.ac.kr>
2022-03-23 16:09:17 +0900

    [Add] Freeboard Router 연결

--- back/src/app.js
+++ back/src/app.js
@@ -5,6 +5,7 @@ import { certificateAuthRouter } from './routers/certificateRouter';
 import { educationRouter } from './routers/educationRouter';
 import { awardRouter } from './routers/awardRouter';
 import { projectRouter } from './routers/projectRouter';
+import { freeboardRouter } from './routers/freeboardRouter';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 
 const app = express();
@@ -29,6 +30,7 @@ app.use(certificateAuthRouter);
 app.use(educationRouter);
 app.use(awardRouter);
 app.use(projectRouter);
+app.use(freeboardRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
