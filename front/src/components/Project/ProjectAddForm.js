4558cf73fe1f4c46ddba655cfc168ed315f3e883
YounJoo Seog <reina.seog@gmail.com>
2022-03-23 21:53:35 +0900

    [Fix] ProjectAddForm 코드 리팩토링 끝

--- front/src/components/Project/ProjectAddForm.js
+++ front/src/components/Project/ProjectAddForm.js
@@ -3,54 +3,55 @@ import { Button, Col, Form, Row } from 'react-bootstrap';
 import * as Api from '../../api';
 import DatePicker from 'react-datepicker';
 
-function ProjectAddForm({ portfolioOwnerId, setProjects, setIsAdding }) {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [fromDate, setFromDate] = useState(new Date());
-  const [toDate, setToDate] = useState(new Date());
+function ProjectAddForm({ projects, setProjects, portfolioOwnerId, setIsAdding }) {
+  const [tempProject, setTempProject] = useState({
+    title: '',
+    description: '',
+    from_date: new Date(),
+    to_date: new Date(),
+  });
+
+  const handleProjectValue = (name, value) => {
+    setTempProject(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleCleandate = cleanDate => {
+    return cleanDate.toISOString().split('T')[0];
+  };
+
+  const from_date = handleCleandate(tempProject.from_date);
+  const to_date = handleCleandate(tempProject.to_date);
 
   const handleSubmit = async e => {
     e.preventDefault();
-
-    const user_id = portfolioOwnerId;
-    // console.log('portfolioOwnerId', portfolioOwnerId);
-
-    const Cleandate = cleanDate => {
-      return cleanDate.toISOString().split('T')[0];
-    };
-
-    // console.log(Cleandate(fromDate));
-    // console.log(Cleandate(toDate));
-
-    const from_date = Cleandate(fromDate);
-    const to_date = Cleandate(toDate);
-
-    await Api.post('project/create', {
-      user_id,
-      title,
-      description,
-      from_date,
-      to_date,
-    });
-
-    await Api.get('Projectlist', user_id).then(res => setProjects(res.data));
-    setIsAdding(false);
+    try {
+      const res = await Api.post('project/create', {
+        user_id: portfolioOwnerId,
+        from_date,
+        to_date,
+        ...tempProject,
+      });
+      setProjects(prev => ({ ...prev, ...res.data }));
+      setIsAdding(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group controlId='ProjectAddTitle' className='mt-3'>
-        <Form.Control type='text' placeholder='프로젝트 이름' value={title} onChange={e => setTitle(e.target.value)} />
+        <Form.Control type='text' placeholder='프로젝트 이름' name='title' value={tempProject.title} onChange={e => handleProjectValue(e.target.name, e.target.value)} />
       </Form.Group>
       <Form.Group controlId='ProjectAddMajor' className='mt-3'>
-        <Form.Control type='text' placeholder='상세내역' value={description} onChange={e => setDescription(e.target.value)} />
+        <Form.Control type='text' placeholder='상세내역' name='description' value={tempProject.description} onChange={e => handleProjectValue(e.target.name, e.target.value)} />
       </Form.Group>
       <Form.Group as={Row} className='mt-3'>
         <Col xs='auto'>
-          <DatePicker selected={fromDate} dateFormat='yyyy-MM-dd' onChange={date => setFromDate(date)} />{' '}
+          <DatePicker selected={tempProject.from_date} name='from_date' dateFormat='yyyy-MM-dd' onChange={date => handleProjectValue('from_date', date)} />{' '}
         </Col>
         <Col xs='auto'>
-          <DatePicker selected={toDate} dateFormat='yyyy-MM-dd' onChange={date => setToDate(date)} />
+          <DatePicker selected={tempProject.to_date} dateFormat='yyyy-MM-dd' name='to_date' onChange={date => handleProjectValue('to_date', date)} />
         </Col>
       </Form.Group>
       <Form.Group as={Row} className='mt-3 text-center'>
