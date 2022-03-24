bd02541022c4b43f6f057d082838199873be67aa
Seohyun-Roh <radult951@gmail.com>
2022-03-25 01:07:40 +0900

    [Create] Skeletons.js

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState, useEffect, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Container, Col, Row } from 'react-bootstrap';
-import Skeleton from 'react-loading-skeleton';
+import { Container, Col, Row, Placeholder } from 'react-bootstrap';
 
 import { UserStateContext } from '../App';
 import * as Api from '../api';
@@ -10,6 +9,7 @@ import Educations from './education/Educations';
 import Awards from './award/Awards';
 import Projects from './Project/Projects';
 import Certificates from './certificate/Certificates';
+import { PortfolioSkeleton } from './Skeletons';
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -59,12 +59,7 @@ function Portfolio() {
   }, [params, userState, navigate]);
 
   if (!isFetchCompleted) {
-    return (
-      <div style={{ height: '90vh', width: '90vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <h2>Loading..</h2>
-        <Skeleton height={100} width={100} style={{ clipPath: 'polygon(0 40%, 60% 40%, 60% 10%, 100% 50%, 60% 90%, 60% 60%, 0 60%)' }} />
-      </div>
-    );
+    return <>{PortfolioSkeleton()}</>;
   }
 
   return (
---
+++ front/src/components/Skeletons.js
@@ -0,0 +1,211 @@
+import React from 'react';
+import { Container, Row, Col, Card, Placeholder } from 'react-bootstrap';
+
+export const HeaderSkeleton = () => {
+  return (
+    <>
+      <Placeholder size='lg' animation='glow' style={{ width: '25%', marginLeft: '20px', marginTop: '15px' }} />
+      <Placeholder size='lg' animation='glow' style={{ width: '5%', position: 'absolute', right: '9%', marginTop: '15px' }} />
+      <Placeholder size='lg' animation='glow' style={{ width: '5%', position: 'absolute', right: '20px', marginTop: '15px' }} />
+    </>
+  );
+};
+
+export const UserSkeleton = () => {
+  return (
+    <Card className='mb-2 ms-3 mr-5' style={{ width: '18rem' }}>
+      <Card.Body>
+        <Row className='justify-content-md-center'>
+          <Placeholder style={{ width: '10rem', height: '8rem' }} className='mb-3' />
+        </Row>
+        <Placeholder as={Card.Title} animation='glow'>
+          <Placeholder xs={5} size='lg' />
+        </Placeholder>
+        <Placeholder as={Card.SubTitle} animation='glow' className='mb-2'>
+          <Placeholder xs={7} />
+        </Placeholder>
+        <Placeholder as={Card.Text} animation='glow' className='mb-2'>
+          <Placeholder xs={10} size='lg' />
+        </Placeholder>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const EducationSkeleton = () => {
+  return (
+    <Card className='mb-3'>
+      <Card.Body>
+        <Placeholder as={Card.Title} animation='glow'>
+          <Placeholder xs={1} size='lg' />
+        </Placeholder>
+        <Card.Text>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={2} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow' className='mb-3'>
+                <Placeholder xs={1} /> <Placeholder xs={1} />
+              </Placeholder>
+            </Col>
+          </Row>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={2} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} /> <Placeholder xs={1} />
+              </Placeholder>
+            </Col>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const AwardSkeleton = () => {
+  return (
+    <Card className='mb-3'>
+      <Card.Body>
+        <Placeholder as={Card.Title} animation='glow'>
+          <Placeholder xs={1} size='lg' />
+        </Placeholder>
+        <Card.Text>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow' className='mb-3'>
+                <Placeholder xs={2} />
+              </Placeholder>
+            </Col>
+          </Row>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow' className='mb-3'>
+                <Placeholder xs={2} />
+              </Placeholder>
+            </Col>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const ProjectSkeleton = () => {
+  return (
+    <Card className='mb-3'>
+      <Card.Body>
+        <Placeholder as={Card.Title} animation='glow'>
+          <Placeholder xs={1} size='lg' />
+        </Placeholder>
+        <Card.Text>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={2} />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow' className='mb-3'>
+                <Placeholder xs={1} /> <Placeholder xs={1} />
+              </Placeholder>
+            </Col>
+          </Row>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={2} />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow' className='mb-3'>
+                <Placeholder xs={1} /> <Placeholder xs={1} />
+              </Placeholder>
+            </Col>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const CertificateSkeleton = () => {
+  return (
+    <Card className='mb-3'>
+      <Card.Body>
+        <Placeholder as={Card.Title} animation='glow'>
+          <Placeholder xs={1} size='lg' />
+        </Placeholder>
+        <Card.Text>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={2} />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow' className='mb-3'>
+                <Placeholder xs={1} />
+              </Placeholder>
+            </Col>
+          </Row>
+          <Row className='align-items-center'>
+            <Col>
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} size='lg' />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={2} />
+              </Placeholder>
+              <br />
+              <Placeholder as='span' animation='glow'>
+                <Placeholder xs={1} />
+              </Placeholder>
+            </Col>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const PortfolioSkeleton = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col md='3' lg='3'>
+          {UserSkeleton()}
+        </Col>
+        <Col>
+          {EducationSkeleton()}
+          {AwardSkeleton()}
+          {ProjectSkeleton()}
+          {CertificateSkeleton()}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
