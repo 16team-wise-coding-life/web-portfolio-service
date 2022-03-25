fa5e6dccfedf9967d38101a5707270692bb89294
parkjungyoon <tbr06057@naver.com>
2022-03-25 17:47:42 +0900

    [Modify] moment모듈로 시간 format ( 새로 추가되는 시간 오류 )

--- front/src/components/comment/CommentCard.js
+++ front/src/components/comment/CommentCard.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Button, Card, Col, Row } from 'react-bootstrap';
+import moment from 'moment';
 
 function CommentCard({ comment, setIsEditing, isEditable, handleDeleteClick }) {
   return (
-    // Body일까.. Text일까...
     <>
       <Card.Body>
         <Card.Text>
           <Row>
             <Col>
               <span>{comment.name}</span>
+              <dev style={{ color: 'grey' }}> / {moment(comment.created_at).format('YYYY-MM-DD HH:mm:ss')}</dev>
               <br />
               <span className="text-muted">{comment.content}</span>
             </Col>
             {isEditable && (
               <>
                 <Col xs lg="1">
-                  <Button variant="outline-info" size="sm" onClick={() => setIsEditing(true)}>
+                  <Button variant="outline-info" size="sm" className="mt-2" onClick={() => setIsEditing(true)}>
                     편집
                   </Button>
                 </Col>
                 <Col xs lg="1">
-                  <Button variant="outline-danger" size="sm" onClick={() => handleDeleteClick(comment.id)}>
+                  <Button variant="outline-danger" size="sm" className="mt-2" onClick={() => handleDeleteClick(comment.id)}>
                     삭제
                   </Button>
                 </Col>
