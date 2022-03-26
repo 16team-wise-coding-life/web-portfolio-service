c3cf60f02014eade116d1a22a19617a122e8fa39
parkjungyoon <tbr06057@naver.com>
2022-03-26 15:57:24 +0900

    [Add]: 좋아요 조회 API 구현

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -29,6 +29,18 @@ likeRouter.post('/like/create', async (req, res, next) => {
   }
 });
 
+// 좋아요 조회 API
+likeRouter.get('/like/:user_id', async (req, res, next) => {
+  try {
+    const like_id = req.params.user_id;
+    const like = await likeService.getLike({ like_id });
+
+    res.status(200).send(like);
+  } catch (error) {
+    next(error);
+  }
+});
+
 likeRouter.delete('/like/delete', async (req, res, next) => {
   try {
     const { user_id, like_id } = req.body;
