df22f747d8901f83443712c5e3fa20666c2d228c
YounJoo Seog <reina.seog@gmail.com>
2022-03-27 10:44:56 +0900

    [Add] 동적배경 추가

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Container, Col, Row, Placeholder } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 
 import { UserStateContext } from '../App';
 import * as Api from '../api';
